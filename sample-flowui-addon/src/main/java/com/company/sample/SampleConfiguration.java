f2f675d8241708f5c3af8805e3e632519ba5671c
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-11-25 11:45:51 +0400

    Init

---
+++ sample-flowui-addon/src/main/java/com/company/sample/SampleConfiguration.java
@@ -0,0 +1,32 @@
+package com.company.sample;
+
+import io.jmix.core.annotation.JmixModule;
+import io.jmix.core.impl.scanning.AnnotationScanMetadataReaderFactory;
+import io.jmix.eclipselink.EclipselinkConfiguration;
+import io.jmix.ui.UiConfiguration;
+import io.jmix.ui.sys.UiControllersConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Collections;
+
+@Configuration
+@ComponentScan
+@ConfigurationPropertiesScan
+@JmixModule(dependsOn = {EclipselinkConfiguration.class, UiConfiguration.class})
+@PropertySource(name = "com.company.sample", value = "classpath:/com/company/sample/module.properties")
+public class SampleConfiguration {
+
+    @Bean("sample_SampleUiControllers")
+    public UiControllersConfiguration screens(ApplicationContext applicationContext,
+                                              AnnotationScanMetadataReaderFactory metadataReaderFactory) {
+        UiControllersConfiguration uiControllers
+                = new UiControllersConfiguration(applicationContext, metadataReaderFactory);
+        uiControllers.setBasePackages(Collections.singletonList("com.company.sample"));
+        return uiControllers;
+    }
+}
---
+++ sample-flowui-addon/src/test/java/com/company/sample/SampleTestConfiguration.java
@@ -0,0 +1,28 @@
+package com.company.sample;
+
+import io.jmix.core.annotation.JmixModule;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@Import(SampleConfiguration.class)
+@JmixModule(id = "com.company.sample.test", dependsOn = SampleConfiguration.class)
+public class SampleTestConfiguration {
+
+    @Bean
+    @Primary
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.HSQL)
+                .build();
+    }
+}
